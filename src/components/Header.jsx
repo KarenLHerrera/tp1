8a62083f8d51d47ca1d4348a7a85a08ceaad2b73
KarenHerreraDev <karenherrera.dev@gmail.com>
2025-04-20 16:57:22 -0300

    segundo commit, titulo del header

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+   let alumno = "Karen Herrera"
+  return (
+    <div>
+      <h1>Portfolio {alumno}</h1>
+      <hr/>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './Header'
+import Main from './Main'
+import Footer from './Footer'
+
+const Home = () => {
+  return (
+    <div>
+      <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
+    </div>
+  )
+}
+
+export default Home
